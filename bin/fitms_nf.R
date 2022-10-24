bcc731ae1efb0c1c231709155d2713d5cb087a45
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2022-10-24 14:06:56 +0100

    Update fitms_nf.R

--- bin/fitms_nf.R
+++ bin/fitms_nf.R
@@ -28,27 +28,28 @@ names(tab)[names(tab) == "V5"] <- "ALT"
 #  }
 #}
 #tab = tab[to_delete,]
-tab <- tab[ complete.cases( sapply( tab$chr, strtoi ) ), ]
+#tab <- tab[ complete.cases( sapply( tab$chr, strtoi ) ), ]
 
+write.csv(tab, paste0(sample,'_fitms_input.csv'))
 
 #optinal read in and out tab
 #tab  <- write.table('C:/Users/Danie/Documents/scanb/MTR/PD31029a/input/PD31029a_caveman_MTR.txt')
 #tab  <- read.table('C:/Users/Danie/Documents/scanb/MTR/PD31029a/input/PD31029a_caveman_MTR.txt')
 
-organ = "Breast"
-genome.v  ="hg38"
+#organ = "Breast"
+#genome.v  ="hg38"
 #print(head(tab))
-res <- tabToSNVcatalogue(tab, genome.v)
-df <- data.frame(res$catalogue)
-write.csv(df, paste0(sample,'_catalogue.csv'))
+#res <- tabToSNVcatalogue(tab, genome.v)
+#df <- data.frame(res$catalogue)
+#write.csv(df, paste0(sample,'_catalogue.csv'))
 
 
-res <-FitMS(catalogues = df, 
-           organ =organ, 
-           exposureFilterType="giniScaledThreshold",
-           useBootstrap = TRUE, 
-           nboot = 200)
+#res <-FitMS(catalogues = df, 
+#           organ =organ, 
+#           exposureFilterType="giniScaledThreshold",
+#           useBootstrap = TRUE, 
+#           nboot = 200)
 
-plotSubsSignatures(signature_data_matrix = df,output_file = paste0(sample, "_SNV_catalogues.pdf"))
+#plotSubsSignatures(signature_data_matrix = df,output_file = paste0(sample, "_SNV_catalogues.pdf"))
 
 #plotFitMS(res, paste0(corepath, '/', sample, sep= ""))
