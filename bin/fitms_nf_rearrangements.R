1756c03f687f586eb351a2b5c730cb0ec572765d
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2022-11-10 16:22:18 +0000

    Update fitms_nf_rearrangements.R

--- bin/fitms_nf_rearrangements.R
+++ bin/fitms_nf_rearrangements.R
@@ -3,58 +3,34 @@ library(VariantAnnotation)
 library(signature.tools.lib)
 args = commandArgs(trailingOnly=TRUE)
 sample <- args[1]
-mtr_input <- args[2]
-#fileConn<-file("output.txt")
-#writeLines(c(sample), fileConn)
-#close(fileConn)
+sv <- args[2]
 
-#read in the input file to mutationtimer
-tab  <- read.table(mtr_input, sep='\t')
-#change the format of the input file to that of FitMS
-tab<- tab[,c(1,2,4,5)]
-names(tab)[names(tab) == "V1"] <- "chr"
-names(tab)[names(tab) == "V2"] <- "position"
-names(tab)[names(tab) == "V4"] <- "REF"
-names(tab)[names(tab) == "V5"] <- "ALT"
+##read in the bedpe file
+sv_bedpe <- read.table(sv,sep = "\t",header = TRUE,
+                     stringsAsFactors = FALSE,check.names = FALSE)
+#build a catalogue from the bedpe file
+res.cat <- bedpeToRearrCatalogue(sv_bedpe)
 
-chroms <- c("1",  "10","11", "12", "13", "14", "15", "16", "17", "18", "19", "2",  "20", "21", "22", "3",  "4" , "5", "6",  "7",  "8","9" ,"X", "Y")
-
-
-for(i in 1:nrow(tab)){
-  if(is.element(tab$chr[i], chroms) != TRUE){
-    tab <- tab[-c(i), ]
-  }
-}
-#to_delete <- list()
-#for(i in 1:length(tab)){
-#  if(is.na(suppressWarnings(as.numeric(as.character(tab$chr[i])))) == TRUE){
-#  to_delete <- append(to_delete, i)
-#  }
-#}
-#tab = tab[to_delete,]
-#tab <- tab[ complete.cases( sapply( tab$chr, strtoi ) ), ]
-
-#write.csv(tab, paste0(sample,'_fitms_input.csv'))
-
-#optinal read in and out tab
-#tab  <- write.table('C:/Users/Danie/Documents/scanb/MTR/PD31029a/input/PD31029a_caveman_MTR.txt')
-#tab  <- read.table('C:/Users/Danie/Documents/scanb/MTR/PD31029a/input/PD31029a_caveman_MTR.txt')
-
-organ = "Breast"
-genome.v  ="hg38"
-#print(head(tab))
-res <- tabToSNVcatalogue(tab, genome.v)
-df <- data.frame(res$catalogue)
+df <- data.frame(res.cat$catalogue)
 names(df)[names(df) == "catalogue"] <- sample
-write.csv(df, paste0(sample,'_catalogue.csv'))
+write.csv(df, paste0(sample,'_rearrangement_catalogue.csv'))
 
-res <-FitMS(catalogues = df, 
-           organ =organ, 
-           exposureFilterType="giniScaledThreshold",
-           useBootstrap = TRUE, 
-           nboot = 200)
 
-plotSubsSignatures(signature_data_matrix = df,output_file = paste0(sample, "_SNV_catalogues.pdf"))
+#organ = "Breast"
+#genome.v  ="hg38"
+#print(head(tab))
+#res <- tabToSNVcatalogue(tab, genome.v)
+#df <- data.frame(res$catalogue)
+#names(df)[names(df) == "catalogue"] <- sample
+#write.csv(df, paste0(sample,'_catalogue.csv'))
+
+#res <-FitMS(catalogues = df, 
+#           organ =organ, 
+#           exposureFilterType="giniScaledThreshold",
+#           useBootstrap = TRUE, 
+#           nboot = 200)
+
+#plotSubsSignatures(signature_data_matrix = df,output_file = paste0(sample, "_SNV_catalogues.pdf"))
 
 #write.csv(res$exposures, 'exposures.tsv', sep='\t')
-plotFitMS(res, 'results/')
+#plotFitMS(res, 'results/')
