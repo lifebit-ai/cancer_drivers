496c80aa17994a99a87eed820ce6f000e82fd7a5
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2022-11-14 11:06:54 +0000

    Update fitms_nf_rearrangements.R

--- bin/fitms_nf_rearrangements.R
+++ bin/fitms_nf_rearrangements.R
@@ -15,7 +15,7 @@ res.cat <- bedpeToRearrCatalogue(sv_bedpe)
 df <- data.frame(res.cat$rearr_catalogue)
 write.csv(df, paste0(sample,'_rearrangement_catalogue.csv'))
 rownames(df) <- df[[1]]
-df <- df[,-1]
+df <- subset(df, select=-c(1))
 #names(df)[names(df) == "catalogue"] <- sample
 
 #plotRearrSignatures(signature_data_matrix = df,output_file = paste0(sample, "_rearrangement_catalogues.pdf"))
