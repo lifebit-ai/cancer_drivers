d732469df32a9d63c54c3445a259289c970fb974
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2023-03-24 15:49:12 +0000

    Update fitms_nf.R

--- bin/fitms_nf.R
+++ bin/fitms_nf.R
@@ -2,61 +2,90 @@
 library(VariantAnnotation)
 library(signature.tools.lib)
 args = commandArgs(trailingOnly=TRUE)
+
 sample <- args[1]
-mtr_input <- args[2]
-#fileConn<-file("output.txt")
-#writeLines(c(sample), fileConn)
-#close(fileConn)
+snvcat_path <- args[2]
+svcat_path <- args[3]
+cnv_path <- args[4]
+indel_highspecific_path <- args[5]
 
-#read in the input file to mutationtimer
-tab  <- read.table(mtr_input, sep='\t')
-#change the format of the input file to that of FitMS
-tab<- tab[,c(1,2,4,5)]
-names(tab)[names(tab) == "V1"] <- "chr"
-names(tab)[names(tab) == "V2"] <- "position"
-names(tab)[names(tab) == "V4"] <- "REF"
-names(tab)[names(tab) == "V5"] <- "ALT"
+genome.v = 'hg38'
+##make the empty input matrix
+col_hrdetect <- c("del.mh.prop", "SNV3", "SV3", "SV5", "hrd", "SNV8")
+input_matrix <- matrix(NA,nrow = 1,ncol = length(col_hrdetect),dimnames = list(sample,col_hrdetect))
+####################################################################
+####indels##########################################################
+####################################################################
+indeltab = read.csv(indel_highspecific_path)
+required_col <- c('chr', 'pos', 'ref', 'alt')
+indeltab <- indeltab[, required_col]
+##rename columns
+colnames(indeltab)[colnames(indeltab) == "pos"] ="position"
+colnames(indeltab)[colnames(indeltab) == "ref"] ="REF"
+colnames(indeltab)[colnames(indeltab) == "alt"] ="ALT"
 
-chroms <- c("1",  "10","11", "12", "13", "14", "15", "16", "17", "18", "19", "2",  "20", "21", "22", "3",  "4" , "5", "6",  "7",  "8","9" ,"X", "Y")
+names(indeltab) <- sample
 
+####################################################################
+####snvs############################################################
+####################################################################
 
-for(i in 1:nrow(tab)){
-  if(is.element(as.character(tab$chr[i]), chroms) != TRUE){
-    tab <- tab[-c(i), ]
-  }
-}
-#to_delete <- list()
-#for(i in 1:length(tab)){
-#  if(is.na(suppressWarnings(as.numeric(as.character(tab$chr[i])))) == TRUE){
-#  to_delete <- append(to_delete, i)
-#  }
-#}
-#tab = tab[to_delete,]
-#tab <- tab[ complete.cases( sapply( tab$chr, strtoi ) ), ]
-
-#write.csv(tab, paste0(sample,'_fitms_input.csv'))
-
-#optinal read in and out tab
-#tab  <- write.table('C:/Users/Danie/Documents/scanb/MTR/PD31029a/input/PD31029a_caveman_MTR.txt')
-#tab  <- read.table('C:/Users/Danie/Documents/scanb/MTR/PD31029a/input/PD31029a_caveman_MTR.txt')
-
-organ = "Breast"
-genome.v  ="hg38"
-#print(head(tab))
-res <- tabToSNVcatalogue(tab, genome.v)
-df <- data.frame(res$catalogue)
-names(df)[names(df) == "catalogue"] <- sample
-write.csv(df, paste0(sample,'_catalogue.csv'))
-
-res <-FitMS(catalogues = df, 
-           organ =organ,
-           #exposureFilterType="giniScaledThreshold",
-           useBootstrap = TRUE, 
-           nboot = 200)
+#snv catalogues
+snvcat = read.csv(snvcat_path)
+rownames(snvcat) <- snvcat$X 
+snvcat$X <- NULL
+names(snvcat)[1] <- sample
 
 
+####################################################################
+####svs############################################################
+####################################################################
+#rearrangements
+svcat <- read.csv(svcat_path)
+rownames(svcat) <- svcat$X 
+svcat$X <- NULL
+names(svcat)[1] <- sample
 
-plotSubsSignatures(signature_data_matrix = df,output_file = paste0(sample, "_SNV_catalogues.pdf"))
+####################################################################
+####cnvs############################################################
+####################################################################
+###make cnv input through modification of [sample]_CNVs.tsv MTR input
+cnvs <- read.csv(cnv_path, sep='\t')
+#make the adjustments
+cnvs['seg_no'] <- 1:nrow(cnvs)
+names(cnvs)[names (cnvs) =='seqnames'] <- 'Chromosome'
+names(cnvs)[names(cnvs) =='start'] <- 'chromStart'
+names(cnvs)[names(cnvs) =='end'] <- 'chromEnd'
+cnvs['total.copy.number.inNormal'] <- rep(2,nrow(cnvs))
+cnvs['minor.copy.number.in Normal'] <- rep(1, nrow(cnvs))
+names(cnvs)[names(cnvs) =='minor_cn'] <- 'minor.copy.number.inTumour'
+cnvs['total.copy.number.inTumour'] <- cnvs['major_cn'] + cnvs['minor.copy.number.inTumour']
+#select only desired columns and put in right order
+col_order <- c('seg_no', 'Chromosome', 'chromStart', 'chromEnd', 'total.copy.number.in Normal', 'minor.copy.number.in Normal', 'total.copy.number.inTumour', 'minor.copy.number.inTumour')
+cnvs <- cnvs[, col_order]
+#output the table and read in the filename to a named vector
+write.table(cnvs, paste0(sample, '_cnvs_for_hrdetect.csv'), sep='\t', row.names=F, quote=F) 
+names(cnvs) <- sample
 
-#write.csv(res$exposures, 'exposures.tsv', sep='\t')
-plotFitMS(res, 'results/')
+
+
+res <- HRDetect_pipeline(input_matrix,
+                         genome.v = genome.v,
+                         SNV_signature_version = "RefSigv2", 
+                         SV_catalogues = svcat,
+                         Indels tab files = indeltab,
+                         CNV tab files = cnvs,
+                         SNV_catalogues = snvcat,
+                         nparallel = 2,
+                         exposureFilter TypeFit = "fixedThreshold", 
+                         gini ThresholdScaling Fit = 10, 
+                         threshold_percentFit = 5,
+                         bootstrapSignatureFit = TRUE, 
+                         nbootFit = 100,
+                         threshold_p.valueFit = 0.05,
+                         bootstrapHRDetectScores = TRUE)
+
+df <- as.data.frame(res$hrdetect_output)
+df['sample'] = sample
+write.table(df, paste0(sample + '_hr_detect.tsv'),sep='\t', row.names = F,quote=F)
+
